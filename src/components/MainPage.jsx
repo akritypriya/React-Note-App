a2f3cf7fe4231a2f8d3976dac7b8cf6761fb6263
Akrity Priya <akritipriya1905@gamil.com>
2024-11-07 19:31:49 +0530

    MainPage complete

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,28 @@
+import "../index.css";
+import styles from "./mainPage.module.css";
+import logo from "../assets/mainlogo.png";
+import lock from "../assets/lock.svg";
+
+
+function MainPage() {
+  return (
+    <div className={styles.mainSection}>
+    <div className={styles.leftSection}>
+        <h3>Pocket Notes</h3>
+        <button>+</button>
+    </div>
+    <div className={styles.rightSection}>
+    <img src={logo} alt="" />
+      <h1>Pocket Notes</h1>
+      <p>Send and receive messages without keeping your phone online.<br/>
+         Use Pocket Notes on up to 4 linked devices and 1 mobile phone
+      </p>
+      <img src={lock} alt="" /><span>&nbsp;&nbsp;end-to-end encrypted</span>
+    </div>
+    </div>
+ )
+}
+
+export default MainPage;
+
+
